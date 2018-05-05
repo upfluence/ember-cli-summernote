053941f32ad13ceafa498ef85b1d095743113f8d
Philippe Ndiaye <phndiaye@gmail.com>
2018-05-05 15:14:24 +0200

    remove bootstrap and summernote from the node assets

--- index.js
+++ index.js
@@ -4,38 +4,15 @@
 var FastbootTransform = require('fastboot-transform');
 
 module.exports = {
-    name: 'ember-cli-summernote',
-    options: {
-      nodeAssets: {
-        'popper.js': {
-          import: ['dist/umd/popper.js'],
-          processTree(input) {
-           return FastbootTransform(input);
-          }
-        },
-        bootstrap: {
-          import: [
-            'dist/css/bootstrap.css',
-            'dist/js/bootstrap.js'
-          ],
-          processTree(input) {
-           return FastbootTransform(input);
-          }
-        },
-        summernote: {
-          srcDir: 'dist',
-          destDir: 'assets',
-          import: [
-            'summernote-bs4.css',
-            'summernote-bs4.js',
-            'font/summernote.eot',
-            'font/summernote.ttf',
-            'font/summernote.woff'
-          ],
-          processTree(input) {
-           return FastbootTransform(input);
-          }
+  name: 'ember-cli-summernote',
+  options: {
+    nodeAssets: {
+      'popper.js': {
+        import: ['dist/umd/popper.js'],
+        processTree(input) {
+         return FastbootTransform(input);
         }
       }
     }
-  };
+  }
+};
